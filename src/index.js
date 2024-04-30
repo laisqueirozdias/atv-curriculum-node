bcc1d7815a6f8d7b315c86a3470a614bf680d3d4
laisqueirozdias <laisqz@gmail.com>
2024-04-29 22:17:03 -0300

    Second Commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-const express = require('express');
+import express from 'express'
+import 'dotenv/config'
 const resumeRoute = require('./route/resumeRoute');
 const app = express();
+import models, {sequelize} from './model/config';
+
 app.use(express.json());
 
+app.use(async (req, res, next) => {
+    req.context = {
+      models,
+    };
+    next();
+  })
+
 app.use('/', resumeRoute);
+
+const eraseDatabaseOnSync = process.env.ERASE_DATABASE_ON_SYNC;
+
+sequelize.sync({force: eraseDatabaseOnSync})
+
 app.listen(3000, () =>{
     console.log('Server on port 3000');
 })
